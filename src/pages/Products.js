4db4d15e5b91505f9dfa71afd1f7a12fe411b353
sanket <sammane228@gmail.com>
2022-11-06 18:17:01 +0530

    ui change

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,44 +1,40 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { baseUrl } from '../API/url';
-import Product from '../component/Product';
-import Sidebar from '../Layout/Sidebar';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllProducts } from '../fetures/cartSlice';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { baseUrl } from "../API/url";
+import Product from "../component/Product";
+import Sidebar from "../Layout/Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProducts } from "../fetures/cartSlice";
 
 const Products = () => {
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
   const [allProducts, setAllProducts] = useState([]);
-  const filterPrice = useSelector(state => state.cart.filterPrice);
+  const filterPrice = useSelector((state) => state.cart.filterPrice);
 
   useEffect(() => {
-    axios.get(baseUrl)
-      .then(data => {
-        setAllProducts(data.data.products)
+    axios
+      .get(baseUrl)
+      .then((data) => {
+        setAllProducts(data.data.products);
         dispatch(getAllProducts(data.data.products));
       })
-      .catch(error => console.log(error))
-  }, [])
-
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
-    <div className='home-container'>
-      <Sidebar />
-      <div className='common products'>
-        <div className="container">
-          <ul className='products-list'>
-            {
-              allProducts.map((product) => {
-                return (
-                  <Product key={product.id} {...product} />
-                )
-              })
-            }
+    <div className="home-container">
+      <div className="container">
+        <Sidebar />
+        <div className="common products">
+          <ul className="products-list">
+            {allProducts.map((product) => {
+              return <Product key={product.id} {...product} />;
+            })}
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
